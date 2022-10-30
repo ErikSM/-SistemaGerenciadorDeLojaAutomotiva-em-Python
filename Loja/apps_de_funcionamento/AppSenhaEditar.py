c2d11175af5c600b841e8e2f7e29e6d4291d3b0b
Erik Miyajima <50684425+ErikSM@users.noreply.github.com>
2022-10-30 21:28:31 +0900

    Add files via upload

--- Loja/apps_de_funcionamento/AppSenhaEditar.py
+++ Loja/apps_de_funcionamento/AppSenhaEditar.py
@@ -1,6 +1,7 @@
-from future.moves import tkinter
+import tkinter
 
-from entrada_de_dados.criar_e_adicionar_senha_na_loja import adicionar_nova_senha_criada_no_arquivo_da_loja
+from entrada_de_dados.criar_e_adicionar_senha_na_loja import adicionar_nova_senha_criada_no_arquivo_da_loja, \
+    apagar_senha_criada_no_arquivo_da_loja
 from estrutura.AppSenha import AppSenha
 from estrutura.Loja import Loja
 
@@ -14,6 +15,10 @@ class AppSenhaEditar(AppSenha):
 
         self.botao_adicionar.config(text="  Editar...", bd=3, command=self.modificar_senha)
 
+        self.botao_remover = tkinter.Button(self.frame_dados, text="Deletar atual", bd=3,
+                                            command=self.deletar_senha_atual)
+        self.botao_remover.grid(row=0, column=3)
+
         self.texto_relatorio.config(state=tkinter.NORMAL)
         self.texto_relatorio.insert(1.0, "\n   Digite a senha no campo acima para registra-la!!")
         self.texto_relatorio.config(state=tkinter.DISABLED)
@@ -26,5 +31,15 @@ class AppSenhaEditar(AppSenha):
         self.texto_relatorio.config(state=tkinter.DISABLED)
 
         adicionar_nova_senha_criada_no_arquivo_da_loja(self.loja)
+
+        self.window.destroy()
+
+    def deletar_senha_atual(self):
+        self.texto_relatorio.config(state=tkinter.NORMAL)
+        self.texto_relatorio.delete(1.0, "end")
+        self.texto_relatorio.insert(1.0, "\n   !!    Senha Deletada !!   ")
+        self.texto_relatorio.config(state=tkinter.DISABLED)
+
+        apagar_senha_criada_no_arquivo_da_loja(self.loja)
 
         self.window.destroy()
