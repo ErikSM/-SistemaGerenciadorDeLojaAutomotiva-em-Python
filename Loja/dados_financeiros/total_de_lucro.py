2b87c617831c2b92d5a99ae8ac4c69c6c5f9bbc4
Erik Miyajima <50684425+ErikSM@users.noreply.github.com>
2022-11-15 22:24:32 +0900

    Add files via upload

--- Loja/dados_financeiros/total_de_lucro.py
+++ Loja/dados_financeiros/total_de_lucro.py
@@ -1,14 +1,13 @@
 from entrada_de_dados.lista_de_vendas_efetuadas import vendas_registradas
 
 
-def calcular_lucro_total_de_vendas():
-
+def criar_relatorio_de_lucro_total_de_vendas():
     total_venda = _calcular_total_bruto_de_vendas()
     total_gasto = _calcular_gastos_totais_com_obtencao_de_veiculos()
+    total_comissao = _calcular_valores_pagos_em_comissao_sobre_a_venda()
+    resultado = _calcular_lucro_total_de_vendas()
 
-    resultado = (int(total_venda) - int(total_gasto))
-
-    return _escrever_relatorio_de_lucro(total_venda, total_gasto, resultado)
+    return _escrever_relatorio_de_lucro(total_venda, total_gasto, total_comissao, resultado)
 
 
 def _calcular_total_bruto_de_vendas():
@@ -16,7 +15,7 @@ def _calcular_total_bruto_de_vendas():
     for i in vendas_registradas:
         variavel = i.preco.split()
         venda_sem_espaco = "".join(variavel)
-        venda = int(venda_sem_espaco)
+        venda = float(venda_sem_espaco)
         total_venda += venda
 
     return total_venda
@@ -27,21 +26,39 @@ def _calcular_gastos_totais_com_obtencao_de_veiculos():
     for i in vendas_registradas:
         variavel = i.veiculo.valor_de_aquisicao.split()
         gasto_sem_espaco = "".join(variavel)
-        gasto = int(gasto_sem_espaco)
+        gasto = float(gasto_sem_espaco)
         total_gasto += gasto
 
     return total_gasto
 
 
-def _escrever_relatorio_de_lucro(total_venda, total_gasto, resultado):
+def _calcular_valores_pagos_em_comissao_sobre_a_venda():
+    total_comissao = 0
+    for i in vendas_registradas:
+        comissao = i.comissao_sobre_a_venda
+        total_comissao += comissao
+
+    return total_comissao
+
+
+def _calcular_lucro_total_de_vendas():
+    total_lucro = 0
+    for i in vendas_registradas:
+        lucro = float(i.lucro_sobre_a_venda)
+        total_lucro += lucro
+    return total_lucro
+
+
+def _escrever_relatorio_de_lucro(total_venda, total_gasto, total_comissao, resultado):
     return f'\n' \
-           f'{" "*10}(( Relatorio de Lucro ))         \n' \
+           f'{" " * 10}(( Relatorio de Lucro ))         \n' \
            f'\n' \
            f'\n' \
-           f'>> Valor bruto ganho em vendas:               R${total_venda}\n' \
-           f'>> Despesas com obtencao de veiculos:         R${total_gasto}\n' \
+           f'>> Valor bruto ganho em vendas:               R${total_venda:.2f}\n' \
+           f'>> Despesas com obtencao de veiculos:         R${total_gasto:.2f}\n' \
+           f'>> Valor pago em comissao de vendas:          R${total_comissao:.2f}\n'\
            f'\n                                              __________\n' \
-           f'     ** Lucro Total com Vendas:                R${resultado}\n' \
+           f'     ** Lucro Total com Vendas:                R${resultado:.2f}\n' \
            f'\n' \
-           f'{"-"*98}' \
+           f'{"-" * 98}' \
            f'\n'
